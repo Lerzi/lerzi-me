24d8aaa30fa387e559d296ba026b0b8ac007a163
lerzi <569259131@qq.com>
2022-05-25 00:19:23 +0800

    feat:style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <main class="text-gray-700 dark:text-gray-200">
+  <main class="text-gray-700 dark:text-gray-200" h-full>
     <Header />
-    <div flex="~" flex-col justify-center items-center>
+    <div flex="~" flex-col items-center h-full>
       <RouterView pt-10 w-250 />
     </div>
 
