f78896f5dbd0b1a59317d293279c68b6d2985f1f
lerzi <569259131@qq.com>
2022-05-30 17:32:03 +0800

    feat:about me

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,10 +2,25 @@
 </script>
 
 <template>
-  <div>
+  <div flex="~ col" gap-5>
     <h1 text-4xl mb-5>
       LERZI
     </h1>
+    <div>
+      Yo! 我是黄顺，一个前端工程师
+    </div>
+    <div>
+      主要使用技术栈是<span px2 text-xl text-pink-600 font-bold>Vue</span>
+    </div>
+    <div>
+      如果你想看我的个人项目，可以
+      <router-link to="/projects" px2 hover:text-pink-600 font-bold delay-100>
+        点击这里
+      </router-link>
+    </div>
+    <!-- <div>
+      你可以通过添加我的 微信 与我取得联系，但请说明来意
+    </div> -->
   </div>
 </template>
 <!--
