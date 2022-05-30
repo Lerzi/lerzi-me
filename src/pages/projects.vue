8db34e5e4b6342f5cb16ef6df3c472565d734df8
lerzi <569259131@qq.com>
2022-05-30 18:21:18 +0800

    feat:update

---
+++ src/pages/projects.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div flex="~ col" gap-10 prose>
+    <h1>
+      项目
+    </h1>
+    <div flex="~" w-full gap-50>
+      <div flex="~" />
+    </div>
+  </div>
+</template>
