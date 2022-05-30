e5235789a29570c130539c5ed85b73efb8293f3a
lerzi <569259131@qq.com>
2022-05-30 21:05:08 +0800

    feat:update

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
@@ -7,8 +6,16 @@
     <h1>
       项目
     </h1>
-    <div flex="~" w-full gap-50>
-      <div flex="~" />
+    <div flex="~" w-full gap-10>
+      <div flex="~ col" text-gray justify-center items-center p-5 gap-3 rounded-sm border-gray-200 hover:bg-gray-50>
+        <div i-mdi-snake w-16 h-16 py-2 />
+        <div text-xl font-bold w-full>
+          贪吃蛇
+        </div>
+        <div text-sm w-full>
+          一个贪吃蛇小游戏
+        </div>
+      </div>
     </div>
   </div>
 </template>
