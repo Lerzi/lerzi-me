398c4730709f5ea086a7baafed1320d589c06c2d
lerzi <569259131@qq.com>
2022-05-30 17:32:26 +0800

    feat:hide article

--- src/components/Menus.vue
+++ src/components/Menus.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
 const menus = ref([
   {
-    title: '文章',
-    path: '/article',
-  },
-  {
     title: '项目',
-    path: '/project',
+    path: '/projects',
   },
   {
     title: '关于我',
@@ -19,7 +15,7 @@ const menus = ref([
   <div flex="~" items-center justify-center gap-2>
     <router-link
       v-for="item in menus" :key="item.path" :to="item.path"
-      icon-btn flex="~" items-center justify-center h-full w-20 transition hover:bg-gray100
+      icon-btn flex="~" items-center justify-center h-full w-20 transition hover:bg-gray100 delay-100
     >
       {{ item.title }}
     </router-link>
